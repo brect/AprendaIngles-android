6aa67023198e6b63416d38f8d528b57c45acf674
brect <0uNn44t1Z8Gd>
2020-01-13 22:58:37 -0300

    Metodos criados para executar audio de bichos e numeros;

--- app/src/main/java/com/blimas/aprendaingles/Fragments/BichosFragment.java
+++ app/src/main/java/com/blimas/aprendaingles/Fragments/BichosFragment.java
@@ -1,6 +1,7 @@
 package com.blimas.aprendaingles.Fragments;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.blimas.aprendaingles.R;
 
-public class BichosFragment extends Fragment {
+public class BichosFragment extends Fragment implements View.OnClickListener{
 
+    private ImageButton btnCao, btnGato, btnLeao,
+                        btnMacaco, btnOvelha, btnVaca;
+
+
+    private MediaPlayer mediaPlayer;
 
     public BichosFragment() {
         // Required empty public constructor
@@ -23,11 +30,86 @@ public class BichosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         View view =  inflater.inflate(R.layout.fragment_bichos, container, false);
 
+
+        btnCao = view.findViewById(R.id.imageCao);
+        btnGato = view.findViewById(R.id.imageGato);
+        btnLeao = view.findViewById(R.id.imageLeao);
+        btnMacaco = view.findViewById(R.id.imageMacaco);
+        btnOvelha = view.findViewById(R.id.imageOvelha);
+        btnVaca = view.findViewById(R.id.imageVaca);
+
+        btnCao.setOnClickListener(this);
+        btnGato.setOnClickListener(this);
+        btnLeao.setOnClickListener(this);
+        btnMacaco.setOnClickListener(this);
+        btnOvelha.setOnClickListener(this);
+        btnVaca.setOnClickListener(this);
 
         return view;
     }
 
+
+    //metodo passado de forma global dentro da classe
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imageCao :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.dog);
+                executaSom();
+                break;
+
+            case R.id.imageGato :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cat);
+                executaSom();
+                break;
+
+            case R.id.imageLeao :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.lion);
+                executaSom();
+                break;
+
+            case R.id.imageMacaco :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.monkey);
+                executaSom();
+                break;
+
+                case R.id.imageOvelha :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.sheep);
+                executaSom();
+                break;
+
+            case R.id.imageVaca :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cow);
+                executaSom();
+                break;
+        }
+
+    }
+
+
+    public void executaSom(){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+
+            //libera recursos utilizados apos finalizar o som
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
--- app/src/main/java/com/blimas/aprendaingles/Fragments/NumerosFragment.java
+++ app/src/main/java/com/blimas/aprendaingles/Fragments/NumerosFragment.java
@@ -1,6 +1,7 @@
 package com.blimas.aprendaingles.Fragments;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.blimas.aprendaingles.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class NumerosFragment extends Fragment {
 
+public class NumerosFragment extends Fragment implements View.OnClickListener {
+
+    private ImageButton btnUm, btnDois, btnTres,
+                        btnQuatro, btnCinco, btnSeis;
+
+    private MediaPlayer mediaPlayer;
 
     public NumerosFragment() {
         // Required empty public constructor
@@ -25,8 +29,87 @@ public class NumerosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        btnUm = view.findViewById(R.id.imageNumeroUm);
+        btnDois = view.findViewById(R.id.imageNumeroDois);
+        btnTres = view.findViewById(R.id.imageNumeroTres);
+        btnQuatro = view.findViewById(R.id.imageNumeroQuatro);
+        btnCinco = view.findViewById(R.id.imageNumeroCinco);
+        btnSeis = view.findViewById(R.id.imageNumeroSeis);
+
+
+        //aplica eventos de click em numerosFragment
+        btnUm.setOnClickListener(this);
+        btnDois.setOnClickListener(this);
+        btnTres.setOnClickListener(this);
+        btnQuatro.setOnClickListener(this);
+        btnCinco.setOnClickListener(this);
+        btnSeis.setOnClickListener(this);
+
+        return view;
     }
 
+    //metodo passado de forma global dentro da classe
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.imageNumeroUm :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.one);
+                executaSom();
+                break;
+
+            case R.id.imageNumeroDois :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.two);
+                executaSom();
+                break;
+
+            case R.id.imageNumeroTres :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.three);
+                executaSom();
+                break;
+
+            case R.id.imageNumeroQuatro :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.four);
+                executaSom();
+                break;
+
+            case R.id.imageNumeroCinco :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.five);
+                executaSom();
+                break;
+
+            case R.id.imageNumeroSeis :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.six);
+                executaSom();
+                break;
+        }
+    }
+
+
+
+    public void executaSom(){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+
+            //libera recursos utilizados apos finalizar o som
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
